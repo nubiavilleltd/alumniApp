a457ed6a73cf5ba18288ec1ffce5d18cce604269
Omololu Jumat <omololujumat@gmail.com>
2026-08-03 16:57:57 +0100

    commence the implementation of permissions setup

---
+++ frontend/src/shared/permissions/base.ts
@@ -0,0 +1,20 @@
+import { AuthSessionUser } from "@/features/authentication/types/auth.types";
+
+export function isSuperAdmin(user:AuthSessionUser){
+    return user.role == "admin"
+}
+export function isFinanceAdmin(user:AuthSessionUser){
+    return user.role == "finances admin"
+}
+export function isContentAdmin(user:AuthSessionUser){
+    return user.role == "content admin"
+}
+export function isEventAdmin(user:AuthSessionUser){
+    return user.role == "event admin"
+}
+export function isApprovalAdmin(user:AuthSessionUser){
+    return user.role == "approval admin"
+}
+export function isStoreKeeperAdmin(user:AuthSessionUser){
+    return user.role == "storekeeper admin"
+}
