0812df291ba3209f04666a0daaecb7825ce85522
Omololu Jumat <omololujumat@gmail.com>
2026-08-13 21:18:34 +0100

    complete the implementation of orders export functionality

--- frontend/src/features/authentication/hooks/useAuth.ts
+++ frontend/src/features/authentication/hooks/useAuth.ts
@@ -1,16 +1,3 @@
-// import { useIdentityStore } from '../stores/useIdentityStore';
-// import { useTokenStore } from '../stores/useTokenStore';
-
-// export const useAuth = () => {
-//   const user = useIdentityStore((s) => s.user);
-//   const accessToken = useTokenStore((s) => s.accessToken);
-//   return {
-//     user,
-//     isAuthenticated: !!user && !!accessToken,
-//     isAdmin: user?.role === 'admin',
-//   };
-// };
-
 import { useIdentityStore } from '../stores/useIdentityStore';
 import { useTokenStore } from '../stores/useTokenStore';
 
